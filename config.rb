eae9bb274956c25317e4b6faa91c41f86a27e3d6
lilinchun <yxkj@163.com>
2014-10-16 10:23:25 +0800

    add sourcemap

--- config.rb
+++ config.rb
@@ -7,8 +7,10 @@ sass_dir = "/v11/sass"
 images_dir = "/v11/img"
 javascripts_dir = "/v11/js"
 
+# enable sourcemaps
 enable_sourcemaps = true
 sass_options = {:sourcemap => true}
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -18,7 +20,6 @@ sass_options = {:sourcemap => true}
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
